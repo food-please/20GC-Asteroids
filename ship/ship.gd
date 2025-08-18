faa5a3b072d3847a954fb43b8e01fb744492fc4b
food-please <food.please.dev@gmail.com>
2025-08-18 11:37:52 -0300

    Make Ships bounce and handle more smoothly

--- ship/ship.gd
+++ ship/ship.gd
@@ -1,9 +1,46 @@
 class_name Ship extends CharacterBody2D
 
+const MAX_COLLISIONS_PER_FRAME: = 4
+
+## Determines how much the ship will resist knockback from damage and collisions.
+## The higher the value, the greater the resistance.
+@export_range(0.0, 1.0, 0.02) var knockback_resist: = 0.0
+
+@export var max_linear_speed: = 560.0
+
+## Determines how the object is currently rotating each frame. Positive is clockwise.
+## Note that the object's velocity and current facing aren't necessarily related. This is usually
+## only relevant when forward (or backwards) thrust is applied.
+var angular_velocity: float = 0.0
+
 
 @onready var anim: AnimationPlayer = $AnimationPlayer
 @onready var hit_sound: AudioStreamPlayer = $HitSounds
 @onready var projectile_origin: Node2D = $ProjectileOrigin
+
+
+func _physics_process(delta: float) -> void:
+	var num_collisions = 0
+	var knockback: = Vector2.ZERO
+	
+	var collision: = move_and_collide(velocity*delta)
+	while collision != null and num_collisions < MAX_COLLISIONS_PER_FRAME:
+		#print("Vel ", velocity.length(), " Col ", collision.get_collider_velocity().length())
+		var collision_normal: = collision.get_normal()
+		var collision_speed: = velocity.length() + collision.get_collider_velocity().length()
+		
+		knockback += collision_normal * collision_speed * (1.0-knockback_resist)
+		var collider = collision.get_collider()
+		if "collision_repulsion" in collision.get_collider():
+			knockback *= collider.collision_repulsion
+			
+		collision = move_and_collide(collision_normal*collision.get_remainder().length())
+		#print(collision.get_normal())
+		#global_position = Vector2(0,0)
+		num_collisions += 1
+	
+	if num_collisions > 0:
+		velocity = knockback.limit_length(max_linear_speed)
 
 
 func take_hit(hit: Hit) -> void:
--- src/player_controller.gd
+++ src/player_controller.gd
@@ -2,30 +2,34 @@ extends Node
 
 var bullet: = load("res://bullet/bullet.tscn")
 
-@export var linear_acceleration: = 5.0
-@export var angular_acceleration: = PI/10
+@export var linear_acceleration: = 1500.0
 
-var max_speed: = 220.0
-var max_angular_velocity: = PI/2
+@export var max_angular_speed: = PI
+@export var angular_acceleration: = 6*PI
 
-var direction: = Vector2.UP
-var current_angular_speed: = 0.0
-var current_speed: = 0.0
+@onready var ship: = get_parent() as Ship
 
 
-@onready var ship: = get_parent() as CharacterBody2D
+func _ready() -> void:
+	assert(ship, "Player controller must be child of a Ship!")
 
 
-func _physics_process(delta: float) -> void:
-	update_rotation()
-	update_current_speed()
+func _process(delta: float) -> void:
+	update_rotation(delta)
+	update_current_speed(delta)
 	
 	#print("\n", current_angular_speed)
-	direction = direction.rotated(current_angular_speed * delta)
-	ship.rotation = direction.angle()
-	#print(direction.angle())w
-	ship.velocity = direction * current_speed
-	ship.move_and_collide(ship.velocity*delta)
+	#direction = direction.rotated(current_angular_speed * delta)
+	#ship.rotation = direction.angle()
+	##print(direction.angle())w
+	#ship.velocity = direction * current_speed
+	
+	#var num_collisions = 0
+	#var collision: = ship.move_and_collide(ship.velocity*delta)
+	#while collision != null and num_collisions < MAX_COLLISIONS_PER_FRAME:
+		#
+		#num_collisions += 1
+	
 	#var rotation: = Input.get_axis("rotate_cw", "rotate_ccw")
 	#if not is_zero_approx(rotation):
 		#pass
@@ -33,31 +37,38 @@ func _physics_process(delta: float) -> void:
 	#elif r
 
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_released("fire_primary"):
-		var new_bullet = bullet.instantiate()
-		new_bullet.global_position = ship.projectile_origin.global_position
-		new_bullet.fire_velocity = (new_bullet.fire_speed+current_speed)*direction
-		new_bullet.origin_ship = ship
-		Events.projectile_fired.emit(new_bullet)
-
-
-func update_rotation() -> void:
+#func _unhandled_input(event: InputEvent) -> void:
+	#if event.is_action_released("fire_primary"):
+		#var new_bullet = bullet.instantiate()
+		#new_bullet.global_position = ship.projectile_origin.global_position
+		#new_bullet.fire_velocity = (new_bullet.fire_speed+current_speed)*direction
+		#new_bullet.origin_ship = ship
+		#Events.projectile_fired.emit(new_bullet)
+#
+#
+func update_rotation(delta: float) -> void:
 	var rotation_direction: = Input.get_axis("rotate_cw", "rotate_ccw")
 	if is_zero_approx(rotation_direction):
-		current_angular_speed = move_toward(current_angular_speed, 0, max_angular_velocity)
+		ship.angular_velocity = move_toward(ship.angular_velocity, 0, angular_acceleration * delta)
 	
 	else:
-		current_angular_speed = clampf(
-			current_angular_speed + rotation_direction * angular_acceleration,
-			-max_angular_velocity,
-			max_angular_velocity
+		ship.angular_velocity = clampf(
+			ship.angular_velocity + rotation_direction * angular_acceleration * delta,
+			-max_angular_speed,
+			max_angular_speed
 		)
+		print(ship.angular_velocity)
+	
+	ship.rotation += ship.angular_velocity * delta
 
 
-func update_current_speed() -> void:
+func update_current_speed(delta: float) -> void:
 	var throttle_direction: = Input.get_axis("throttle_down", "throttle_up")
-	current_speed = clampf(
-		current_speed + throttle_direction * linear_acceleration,
-		0, 
-		max_speed)
+	if is_zero_approx(throttle_direction):
+		ship.velocity = ship.velocity.move_toward(Vector2.ZERO, linear_acceleration * delta)
+	
+	else:
+		var facing: = Vector2.RIGHT.rotated(ship.rotation)
+		print("\nVel before: ", ship.velocity, " mod: ", ship.velocity.normalized() * throttle_direction * linear_acceleration * delta, " Facing: ", facing)
+		ship.velocity += facing * throttle_direction * linear_acceleration * delta
+		ship.velocity = ship.velocity.limit_length(ship.max_linear_speed)
